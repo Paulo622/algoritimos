17af5910af8597ce6e8fc047292ef4616a1c6d07
Paulo Vitor <Teodoropaulo1504@gmail.com>
2025-08-04 19:42:18 -0300

    Declaração de Variáveis Decimal Formatado

---
+++ 02_Variaveis/DeclaracaoVariaveisDecimalFormatado.cs
@@ -0,0 +1,13 @@
+using System;
+
+class DeclaracaoVariaveisDecimalFormatado
+{
+	static void Main(string[]args)
+	{
+		decimal x = 0,999m;
+		decimal y = 9999999999999999999999999999;
+		Console.WriteLine("Minha quantia = {0:C}",x);
+		Console.WriteLine("Sua quantia = {0:C}",y);
+	}
+
+}
