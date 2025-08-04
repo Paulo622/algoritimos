5ff76c8e3e8ee6538cfc13da22855e989210b909
Paulo Vitor <Teodoropaulo1504@gmail.com>
2025-08-04 20:17:35 -0300

    Correção de letra no final

--- 02_Variaveis/DeclaracaoVariaveisDecimalFormatado.cs
+++ 02_Variaveis/DeclaracaoVariaveisDecimalFormatado.cs
@@ -5,7 +5,7 @@ class DeclaracaoVariaveisDecimalFormatado
 	static void Main(string[]args)
 	{
 		decimal x = 0.999m;
-		decimal y = 9999999999999999999999999999;
+		decimal y = 9999999999999999999999999999m;
 		Console.WriteLine("Minha quantia = {0:C}",x);
 		Console.WriteLine("Sua quantia = {0:C}",y);
 	}
