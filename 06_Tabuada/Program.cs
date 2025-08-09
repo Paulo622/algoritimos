953d1de22894942d1a8d0051a06b0f919b12b04e
Paulo Vitor <Teodoropaulo1504@gmail.com>
2025-08-08 22:09:16 -0300

    Criação do código para Tabuada

---
+++ 06_Tabuada/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _06_Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Digite um número para ver a tabuada: ");
+           int numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nTabuada do {numero}:\n");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{numero} x {i} = {numero * i}");
+            }
+
+            Console.ReadLine();
+        }          
+    }
+}
