215bfc9c104238ab4dc3726a74458d5bff50a6e1
Paulo Vitor <Teodoropaulo1504@gmail.com>
2025-08-11 22:28:32 -0300

    Atividade 07_TempoDownload

---
+++ 07_TempoDownload/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
+namespace _07_TempoDownload
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         double tamanhoMB = 0;
+         double velocidadeMbps = 0;
+         double tempoSegundos, tempoMinutos;
+
+            while (true)
+            {
+                Console.Write("Informe o tamanho do arquivo(MB): ");
+                if (double.TryParse(Console.ReadLine(), out tamanhoMB) && tamanhoMB > 0)
+
+
+                        break;
+                else
+                
+                    Console.WriteLine("Valor inválido! Digite um número maior que zero.");
+
+
+            }
+
+            
+            while (true)
+            {
+                Console.Write("Informe a velocidade da sua conexão (Mbps): ");
+                if (double.TryParse(Console.ReadLine(), out velocidadeMbps) && velocidadeMbps > 0)
+                
+                        break;     
+                else
+                
+                    Console.WriteLine("Valor inválido! Digite um número maior que zero.");
+            }
+
+            tempoSegundos = (tamanhoMB * 8) / velocidadeMbps;
+            tempoMinutos = tempoSegundos / 60;
+             Console.WriteLine($"Tempo aproximado de download :{tempoMinutos: F2} minutos ");
+        
+        }
+    }
+}
